389e34846d5d1e72f53cbb69b1aae1acc3405255
chhatrapl <chhatrpalyadaw9@gmail.com>
2026-02-06 16:15:12 +0530

    signup page set up

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,43 +1,71 @@
-
+import { useState } from "react";
 
 function SignUp() {
+
+     const  [email, setEmail] = useState('')
+     const  [password, setPassword] = useState('')
+     const  [showPassword, setShowPassword] = useState(false);
+
+
   return (
-    <div>
+      
+
+   <div>
     
-    <div class="w-full max-w-[400px] min-h-screen p-8 flex flex-col justify-between">
+    <div className="w-full max-w-100 min-h-screen p-8 flex flex-col justify-between">
         
-        <div class="mt-8 text-center">
-            <div class="inline-flex items-center justify-center w-16 h-16 bg-indigo-600 rounded-full mb-4 shadow-lg shadow-indigo-500/20">
-                <i class="fas fa-headphones-alt text-3xl"></i>
+        <div className="mt-8 text-center">
+            <div className="inline-flex items-center justify-center w-16 h-16 bg-indigo-600 rounded-full mb-4 shadow-lg shadow-indigo-500/20">
+                <i className="fas fa-headphones-alt text-3xl"></i>
             </div>
-            <h1 class="text-3xl font-bold tracking-tight">Join Melodix</h1>
-            <p class="text-zinc-400 mt-2">Stream your rhythm, anywhere.</p>
+            <h1 className="text-3xl font-bold tracking-tight">Join Melodix</h1>
+            <p className="text-zinc-400 mt-2">Stream your rhythm, anywhere.</p>
         </div>
 
-        <form class="space-y-5 mt-10">
+        <form className="space-y-5 mt-10">
         
 
             <div>
-                <label class="block text-xs font-medium uppercase tracking-wider text-zinc-500 mb-1 ml-1">Email Address</label>
-                <input type="email" placeholder="hello@music.com" 
-                    class="w-full bg-zinc-900 text-white border border-zinc-800 rounded-2xl px-5 py-4 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition-all" />
+                <label className="block text-xs font-medium uppercase tracking-wider text-zinc-500 mb-1 ml-1">Email Address</label>
+                <input type="email" placeholder="hello@music.com" name="email"
+                    className="w-full bg-zinc-900 text-white border border-zinc-800 rounded-2xl px-5 py-4 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition-all" 
+                    onChange={function(elem){
+                           const tempE = elem.target.value;
+                           console.log(tempE)
+                           setEmail(tempE)
+                    }}
+                    />
             </div>
 
             <div>
-                <label class="block text-xs font-medium uppercase tracking-wider text-zinc-500 mb-1 ml-1"> Set Password</label>
-                <input type="password" placeholder="••••••••" 
-                    class="w-full bg-zinc-900 border border-zinc-800 text-white rounded-2xl px-5 py-4 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition-all" />
+                <label className="block text-xs font-medium uppercase tracking-wider text-zinc-500 mb-1 ml-1"> Set Password</label>
+                <input type={showPassword? "text":"password"} placeholder="••••••••"  name="password"
+                   className="w-full bg-zinc-900 border border-zinc-800 text-white rounded-2xl px-5 py-4 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition-all" 
+                   onChange={function(elem){
+                          const tempP = elem.target.value;
+                          console.log(tempP)
+                          setPassword(tempP)
+                   }}
+                   />
+                    <button
+                         type="button"
+                      className=" ml-75 text-zinc-400"
+                     onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? "👁️" : "🙈"}
+                    </button>
+                   
             </div>
 
             <button type="submit" 
-                class="w-full bg-indigo-600 hover:bg-indigo-500 text-white font-bold py-4 rounded-2xl mt-4 shadow-lg shadow-indigo-600/30 active:scale-[0.98] transition-transform cursor-pointer">
+               className="w-full bg-indigo-600 hover:bg-indigo-500 text-white font-bold py-4 rounded-2xl mt-4 shadow-lg shadow-indigo-600/30 active:scale-[0.98] transition-transform cursor-pointer">
                 SignUp
             </button>
         </form>
 
-        <div class="mt-10">
-                     <p class="text-center text-zinc-500 text-sm mt-8">
-                Already have an account? <a href="#" class="text-indigo-400 font-semibold hover:underline">Log in</a>
+        <div className="mt-10">
+                     <p className="text-center text-zinc-500 text-sm mt-8">
+                Already have an account? <a href="#" className="text-indigo-400 font-semibold hover:underline">Log in</a>
             </p>
         </div>
 
