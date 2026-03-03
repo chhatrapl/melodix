c0fc48dae1254ccd99d2433abf9d562cc579e791
chhatrapl <chhatrpalyadaw9@gmail.com>
2026-03-03 09:06:38 +0530

    err box modifide

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,21 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import { Bold, Italic } from 'lucide-react'
 
 createRoot(document.getElementById('root')).render(
   
     
   <BrowserRouter>
-  <Toaster position='top-right' reverseOrder={false} />
+  <Toaster containerStyle={{top:'45%'}}  toastOptions={{style:{
+    fontSize:20, 
+    width:500, 
+    height:150,
+    textTransform:'uppercase', 
+    fontWeight:Bold,
+    color:'white',
+    background:'gray'
+    }}} />
   <App />
   </BrowserRouter>
 )
